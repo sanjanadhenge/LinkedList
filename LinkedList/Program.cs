9a1003e186b39200a9006763e48a12e99bfb1abc
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-22 17:24:50 +0530

    [sanjana] Add : CreateLinkedList

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListData linkedListData = new LinkedListData();
+            linkedListData.Add(56);
+            linkedListData.Add(30);
+            linkedListData.Add(70);
+        }
+    }
+}
